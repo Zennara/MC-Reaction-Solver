0668aa987268edb8990401ac3ce20f526e6a15eb
Zennara <64995253+Zennara@users.noreply.github.com>
2023-05-27 02:55:01 -0400

    switch window, log scrambles

--- main.py
+++ main.py
@@ -2,8 +2,16 @@ import time
 import os
 import pyautogui
 import random
+import pygetwindow
 
 log_file_path = os.getenv("APPDATA") + "/.minecraft/logs/latest.log"
+
+# switch to minecraft window automatically
+SWITCH_WINDOW = True
+LOG_UNSCRAMBLES = True
+
+# disable failsafe (allow mouse movement)
+pyautogui.FAILSAFE = False
 
 
 def read_chat_log():
@@ -25,6 +33,34 @@ def read_chat_log():
                     # Reaction » Unscramble the word estt for a random prize!
                     if after_reaction.startswith("Unscramble"):
                         pass
+
+                    # log unscramble words
+                    if LOG_UNSCRAMBLES:
+                        start_string = "unscrambled the word "
+                        end_string = " in "
+
+                        start_index = after_reaction.find(start_string) + len(start_string)
+                        end_index = after_reaction.find(end_string)
+
+                        if start_index != -1 and end_index != -1:
+                            logged_word = after_reaction[start_index:end_index]
+                            if not os.path.isfile('unscrambles.txt'):
+                                # Create the file if it doesn't exist
+                                open('unscrambles.txt', 'a').close()
+
+                            # Read the contents of the text file
+                            with open('unscrambles.txt', 'r') as file:
+                                lines = file.readlines()
+
+                            # Check if the word exists in the file
+                            if logged_word + '\n' not in lines:
+                                # Append the word to a new line in the file
+                                with open('unscrambles.txt', 'a') as file:
+                                    file.write(logged_word + '\n')
+
+                                print(f"The word '{logged_word}' was added to the file.")
+                            else:
+                                print(f"The word '{logged_word}' already exists in the file.")
 
                     # Reaction » Type the word TEST for a random prize!
                     if after_reaction.startswith("Type"):
@@ -60,13 +96,26 @@ def type_macro(text):
     # Sleep for the random delay
     time.sleep(delay)
 
+    if SWITCH_WINDOW:
+        # Find the Minecraft window by its title
+        minecraft_windows = [window for window in pyautogui.getAllTitles() if window.startswith('Minecraft')]
+
+        if minecraft_windows:
+            # Activate the first Minecraft window found
+            minecraft_window = pyautogui.getWindowsWithTitle(minecraft_windows[0])[0]
+            minecraft_window.maximize()
+            minecraft_window.activate()
+        else:
+            print("Minecraft window not found")
+
     # type the answer
     pyautogui.press('t')
     pyautogui.typewrite(text)
     pyautogui.press('enter')
 
-    line = "-"*30
+    line = "-" * 30
     print(f"{line}\nSolved: {text} in {delay}\n{line}")
+
 
 # Call the function to start reading the chat log
 read_chat_log()
